534c07f26434ce4b6103a1cfa07a4f5fcde39aac
Pablo Morales <pm8morales@gmail.com>
2023-10-07 21:47:43 -0700

    update guestbook

--- lifeofpablo-guestbook/www/guestbook/templates/header.php
+++ lifeofpablo-guestbook/www/guestbook/templates/header.php
@@ -5,35 +5,40 @@
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="webmention" href="https://webmention.io/lifeofpablo.com/webmention" />
 	<link href="https://fonts.googleapis.com/css?family=Titillium+Web:Light:300,400,600,700&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined"
       rel="stylesheet">
 	<title>Pablo's Guestbook</title>
-
-<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/light.css"> -->
-
-	<link rel="stylesheet" href="/media/themes/pablo.css">
-	<link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="https://pabs.tech/media/css/tachyons.css">
+    <link rel="webmention" href="https://webmention.io/lifeofpablo.com/webmention" />
+    <link rel="sitemap" type="text/xml" href="/sitemap/page:sitemap.xml">
+    <link rel="stylesheet" href="https://unpkg.com/tachyons@4.12.0/css/tachyons.min.css"/>
+    <script type="text/javascript">
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+          var u="https://matomo.lifeofpablo.com/";
+          _paq.push(['setTrackerUrl', u+'matomo.php']);
+          _paq.push(['setSiteId', '1']);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+      </script>
 <script src="https://kit.fontawesome.com/2ad0b8bc3e.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-<div class="header" role="banner">
-<div class="sitename">
-<h1><a href="/"><i class="sitename-logo"></i>Pablo Morales</a></h1>
-</div>
-<div class="sitename-banner"></div>
-<div class="navigation" role="navigation" aria-label="Main">
-<ul>
-<li><a class="active" aria-current="page" href="/">Home</a></li>
-<li><a href="/blog/">Blog</a></li>
-<li><a href="/photography/">Photography</a></li>
-<li><a href="/projects/">Projects</a></li>
-<li><a href="https://lifeofpablo.com/feed/page:feed.xml"><i class="fa-solid fa-rss"></i></a></li>
-</ul>
-</div>
-<div class="navigation-banner"></div>
+<nav class="pa3 pa4-ns">
+  <a class="link dim black b f6 f5-ns dib mr3" href="/" title="Home">Pablo Morales</a>
+  <a class="link dim gray    f6 f5-ns dib mr3" href="/" title="Home">Home</a>
+  <a class="link dim gray    f6 f5-ns dib mr3" href="/blog" title="Blog">Blog</a>
+  <a class="link dim gray    f6 f5-ns dib mr3" href="/photography" title="Photography">Photos</a>
+  <a class="link dim gray    f6 f5-ns dib" href="/now" title="Now">Now</a>
+  <a class="link dim gray    f6 f5-ns dib" href="/feed/page:feed.xml" title="RSS Feed"><i class="fa-solid fa-rss"></i></a>
+
+</nav>
+
+
 </div>
 	<h1>Guestbook</h1>
