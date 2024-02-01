6b383ab3b07bbe27d7f2893acbb8eca695c0029f
Pablo Morales <pm8morales@gmail.com>
2024-01-31 23:36:04 -0800

    Add california

---
+++ lifeofpablo-interactives/www/california/templates/header.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>California</title>
+
+	<link rel="stylesheet" href="https://unpkg.com/tachyons@4.12.0/css/tachyons.min.css"/>
+	<?php include "extra-header.php"; ?>
+</head>
+
+<body>
+	<nav class="dt w-100 border-box pa3 ph5-ns">
+        <a class="dtc v-mid mid-gray link dim w-25" href="/" title="Home">
+          <img src="https://static.lifeofpablo.com/pabs-cropped.jpg" class="dib w2 h2 br-100" alt="Pablo Morales">
+        </a>
+        <div class="dtc v-mid w-75 tr">
+          <a class="link dim dark-gray f6 f5-ns dib mr3 mr4-ns" href="/blog" title="Blog">Blog</a>
+          <a class="link dim dark-gray f6 f5-ns dib mr3 mr4-ns" href="/photography" title="Photography">Photography</a>
+          <a class="link dim dark-gray f6 f5-ns dib" href="/now" title="Now">Now</a>
+        </div>
+      </nav>
