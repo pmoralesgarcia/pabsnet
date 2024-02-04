bb4fece9918446b250d26e58126e715b843feb63
Pablo Morales <pm8morales@gmail.com>
2024-02-03 22:21:48 -0800

    add personivebecome ghost image to docker compose

--- lifeofpablo-interactives/www/california/index.php
+++ lifeofpablo-interactives/www/california/index.php
@@ -17,36 +17,32 @@
     </div>
   </div>
   <div class="center measure-wide f5 pv5 lh-copy ph2">
-    <h1 class="f1 lh-title">Placeholder</h1>
-    <p>
-      The choice of paper size is one of the  first of any given work to be printed. There are two basic paper size systems in the world: the international A sizes, and the American sizes.
-    </p>
-    <p>
-      The international Standard paper sizes, called the A series, is based
-      on a golden rectangle, the divine proportion. It is extremely handsome
-      and practical as well. It is adopted by many countries around the world
-      and is based on the German DIN metric Standards. The United States uses
-      a basic letter size (8 1/2 x 11”) of ugly proportions, and results in
-      complete chaos with an endless amount of paper sizes. It is a
-      by-product of the culture of free enterprise, competition and waste.
-      Just another example of the misinterpretations of freedom.
-    </p>
-    <p>
-      These are the basic DIN sizes in mm. for : A0, 841x1189 - A1, 594x841 -
-      A2, 420x594 - A3, 297x420 - A4, 210x297 - A5, 148x210 - A6, 105 x148 -
-      A7, 74x 105 - A8, 52x74 - A9, 37x52 - A10, 26x37.
-    </p>
-    <p>
-      The A4 is the basic size for stationary. Two thirds of it is a square,
-      a nice economical happenstance resulting from the golden rectangle.
-      It is one of the reasons we tend to use as much
-      as possible the DIN sizes: proportions are always leading to other nice proportions.
-    </p>
-    <p>
-      This does not happen with the American basic size which leads to nothing. I counted 28 different standard sizes in USA!. The only reason we use it is because everybody in USA uses it, all stationary in USA is that size, so are manilla folders,  les and of ce equipment!
-      The repercussion of ugliness is endless.
-    </p>
-  </div>
+  <div class="athelas pa4">
+  <p class="f4 f4-ns lh-copy measure center">
+    The form of our letters, the older handwriting and inscriptions as much as
+    the cuttings in use today, reflects a convention that has slowly solidified,
+    an agreement hardened in many battles. Even after the Renaissance several
+    European countries retained broken, blackletter national scripts in opposition to roman, the obligatory type for all Latin material; yet even today, I hope, the last word about Fraktur has not been spoken.
+  </p>
+  <p class="f4 f4-ns lh-copy measure center">
+    The form of our letters, the older handwriting and inscriptions as much as
+    the cuttings in use today, reflects a convention that has slowly solidified,
+    an agreement hardened in many battles. Even after the Renaissance several
+    European countries retained broken, blackletter national scripts in opposition to roman, the obligatory type for all Latin material; yet even today, I hope, the last word about Fraktur has not been spoken.
+  </p>
+  <p class="f4 f4-ns lh-copy measure center">
+    Apart from that, the roman minuscule has been our way of writing for hundreds
+    of years. What followed were merely fashionable variations, here and there even
+    deformations, of the noble basic form, but no improvement whatever. The
+    punches of Claude Garamond, cut around 1530 in Paris, are simply unsurpassed in
+    their clarity, readability and beauty. Garamond appeared on the scene at a
+    time when the occidental book, as an object, cast off its medieval
+    ponderousness and took up the form which today is still the best: the slender
+    and upright rectangular body, comprising folded sheets stitched or sewn at
+    the back, in a cover whose protruding edges protect the trimmed pages.
+  </p>
+
+</div>
 </article>
 
 
