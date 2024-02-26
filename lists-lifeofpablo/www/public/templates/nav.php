0d886165c99dd65dd91feaff92cdf74f6e06148d
Pablo Morales <pm8morales@gmail.com>
2024-02-25 20:46:16 -0800

    update nav in app

--- lists-lifeofpablo/www/public/templates/nav.php
+++ lists-lifeofpablo/www/public/templates/nav.php
@@ -1,8 +1,16 @@
-<nav class="ma1 pa1">
-  <a class="link dim black b f6 f5-ns dib mr3" href="/" title="Home">Items</a>
-  <a class="link dim gray    f6 f5-ns dib mr3" href="/" title="Home">Home</a>
-  <a class="link dim gray    f6 f5-ns dib mr3" href="/vinyls/" title="Vinyls">Vinyls</a>
-  <a class="link dim gray    f6 f5-ns dib mr3" href="/books/" title="Books">Books</a>
-  <a class="link dim gray    f6 f5-ns dib" href="/cds/" title="CDs">CDs</a>
+
+
+<nav class="db dt-l w-100 border-box pa3 ph5-l">
+  <a class="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" href="/" title="Home">
+    <img src="https://static.lifeofpablo.com/pabs-cropped.jpg class="dib w2 h2 br-100" alt="Pablo Morales">
+  </a>
+  <div class="db dtc-l v-mid w-100 w-75-l tc tr-l">
+    <a class="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="/index.php" title="Home">Home</a>
+    <a class="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="/music-listened/index.php" title="Music">Music</a>
+    <a class="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="/vinyls/index.php" title="Vinyls">Vinyls</a>
+    <a class="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="https://lifeofpablo.com/blog" title="Blog">Blog</a>
+    <a class="link dim dark-gray f6 f5-l dib" href="https://lifeofpablo.com/now" title="Now">Contact</a>
+  </div>
 </nav>
+
 
