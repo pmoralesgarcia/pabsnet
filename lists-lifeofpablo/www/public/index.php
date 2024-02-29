77cfec1f122db4d08d9972ca6652bfb695ae09a4
Pablo Morales <pm8morales@gmail.com>
2024-02-28 19:09:57 -0800

    fix lists

--- lists-lifeofpablo/www/public/index.php
+++ lists-lifeofpablo/www/public/index.php
@@ -1,7 +1,7 @@
 <?php include "./templates/header.php"; ?>
 Books
 <ul>
-	<?php
+<!-- 	<?php
 	     if($_SERVER['X-Vouch-User'])
 echo '<li><a href="create"><strong>Create Guestbook Entry</strong></a> - Come say hello!</li>';
 else
@@ -20,38 +20,34 @@ echo 'Please sign in using your domain to post on guestbook.';
 ';
 else
 echo '<a href="panel">Panel</a>';
-			?>
+			?> -->
 
 	<li><a href="update"><strong>Read Guestbook Entries</strong></a> -See other visitors posts!</li>
 </ul>
 <ul>
-	<li><a href="books/create.php"><strong>Create</strong></a> - add a user</li>
-	<li><a href="books/read.php"><strong>Read</strong></a> - find a user</li>
-	<li><a href="books/update.php"><strong>Update</strong></a> - edit a user</li>
-	<li><a href="books/"><strong>Vinyls</strong></a> - View Vinyls</li>
+	<li><a href="books/create.php"><strong>Create</strong></a> - add a book</li>
+	<li><a href="books/read.php"><strong>Find</strong></a> - find a book</li>
+	<li><a href="books/update.php"><strong>View</strong></a> - View/edit a book</li>
 </ul>
 CD's
 <ul>
-	<li><a href="cds/create.php"><strong>Create</strong></a> - add a user</li>
-	<li><a href="cds/read.php"><strong>Read</strong></a> - find a user</li>
-	<li><a href="cds/update.php"><strong>Update</strong></a> - edit a user</li>
-	<li><a href="cds/"><strong>Vinyls</strong></a> - View Vinyls</li>
+	<li><a href="cds/create.php"><strong>Create</strong></a> - add a CD</li>
+	<li><a href="cds/read.php"><strong>Find</strong></a> - find a cd</li>
+	<li><a href="cds/update.php"><strong>View</strong></a> - View/edit a cd</li>
 </ul>
 
 Vinyls
 <ul>
-	<li><a href="vinyls/create.php"><strong>Create</strong></a> - add a user</li>
-	<li><a href="vinyls/read.php"><strong>Read</strong></a> - find a user</li>
-	<li><a href="vinyls/update.php"><strong>Update</strong></a> - edit a user</li>
-	<li><a href="vinyls/"><strong>Vinyls</strong></a> - View Vinyls</li>
+	<li><a href="vinyls/create.php"><strong>Create</strong></a> - add a vinyl</li>
+	<li><a href="vinyls/read.php"><strong>Find</strong></a> - find a vinyl</li>
+	<li><a href="vinyls/update.php"><strong>Edit</strong></a> - edit a vinyl</li>
 </ul>
 <h2 class="">Lists</h2>
 Music Listened
 <ul>
-	<li><a href="music-listened/create.php"><strong>Create</strong></a> - add a user</li>
-	<li><a href="music-listened/read.php"><strong>Read</strong></a> - find a user</li>
-	<li><a href="music-listened/update.php"><strong>Update</strong></a> - edit a user</li>
-	<li><a href="music-listened/"><strong>Vinyls</strong></a> - View Vinyls</li>
+	<li><a href="music-listened/create.php"><strong>Create</strong></a> - add a song</li>
+	<li><a href="music-listened/read.php"><strong>Find</strong></a> - find a song</li>
+	<li><a href="music-listened/update.php"><strong>View/edit</strong></a> - edit a song</li>
 </ul>
 
 <?php include "./templates/footer.php"; ?>
--- lists-lifeofpablo/www/public/music-listened/index.php
+++ lists-lifeofpablo/www/public/music-listened/index.php
@@ -1,10 +1,10 @@
 <?php include "../templates/header.php"; ?>
 
 <ul>
-	<li><a href="create.php"><strong>Create</strong></a> - add a user</li>
-	<li><a href="read.php"><strong>Read</strong></a> - find a user</li>
-	<li><a href="update.php"><strong>Update</strong></a> - edit a user</li>
-	<li><a href="delete.php"><strong>Delete</strong></a> - delete a user</li>
+	<li><a href="create.php"><strong>Create</strong></a> - add a song</li>
+	<li><a href="read.php"><strong>Read</strong></a> - search a song</li>
+	<li><a href="update.php"><strong>Update</strong></a> - edit a song</li>
+	<li><a href="delete.php"><strong>Delete</strong></a> - delete a song</li>
 </ul>
 
 <?php include "../templates/footer.php"; ?>
