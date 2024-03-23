a51a4199216ab8210d724efeb6ed9742e72702a5
Pablo Morales <pm8morales@gmail.com>
2024-03-23 10:24:35 -0700

    add profile

---
+++ lifeofpablo-interactives/www/profile/templates/footer.php
@@ -0,0 +1,10 @@
+<footer class="pv4 ph3 ph5-m ph6-l mid-gray">
+  <small class="f6 db tc">© <?php echo date("Y"); ?> <b class="ttu">Pabs Tech</b>., All Rights Reserved</small>
+  <div class="tc mt3">
+  </div>
+</footer>
+
+
+</body>
+
+</html>
