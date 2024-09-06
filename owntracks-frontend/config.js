4dc0b1176a6fc9dbae4741ad16174e8d5185c9f4
Pablo Morales <pm8morales@gmail.com>
2024-09-05 22:21:00 -0700

    add owntracks-frontend to docker-compose

---
+++ owntracks-frontend/config.js
@@ -0,0 +1,8 @@
+// Here you can overwite the default configuration values
+window.owntracks = window.owntracks || {};
+window.owntracks.config = {
+    api: {
+        baseUrl: "https://owntracks.pabs.lol",
+        locale: "en-US"
+      },
+};
