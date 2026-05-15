717d1c707775c792024bc3bc292081158b227d8f
Pablo Morales <pm8morales@gmail.com>
2026-05-14 19:56:26 -0700

    update indieauth selfauth

--- login.lifeofpablo.com/start.sh
+++ login.lifeofpablo.com/start.sh
@@ -1,3 +1,17 @@
-#!/bin/sh
-php-fpm8.2
-nginx -g 'daemon off;'
+#!/bin/bash
+
+# Inject Environment Variables into PHP-FPM
+# This allows getenv() to work inside the container
+FPM_CONF="/etc/php/8.2/fpm/pool.d/www.conf"
+
+echo "env[SA_APP_URL] = '$SA_APP_URL'" >> $FPM_CONF
+echo "env[SA_APP_KEY] = '$SA_APP_KEY'" >> $FPM_CONF
+echo "env[SA_USER_URL] = '$SA_USER_URL'" >> $FPM_CONF
+echo "env[SA_USER_HASH] = '$SA_USER_HASH'" >> $FPM_CONF
+
+# Clear existing log/pid files if they exist from a crash
+rm -f /run/php/php8.2-fpm.pid
+
+# Start services
+service php8.2-fpm start
+nginx -g "daemon off;"
--- login.lifeofpablo.com/www/setup.php
+++ login.lifeofpablo.com/www/setup.php
@@ -1,131 +1,56 @@
-<html>
-<head>
-<title>
-Setup Selfauth
-</title>
-<style>
-h1{text-align:center;margin-top:5%;}
-h2{text-align:center;}
-.instructions{text-align:center;}
-.message{margin-top:20px;text-align:center;font-size:1.2em;font-weight:bold;}
-pre {width:400px; margin-left:auto; margin-right:auto;margin-bottom:50px;}
-form{ 
-margin-left:auto;
-width:300px;
-margin-right:auto;
-text-align:center;
-margin-top:20px;
-border:solid 1px black;
-padding:20px;
-}
-.form-line{ margin-top:5px;}
-.submit{width:100%}
-</style>
-</head>
-<body>
-<h1>Setup Selfauth</h1>
-<div>
 <?php
 define('RANDOM_BYTE_COUNT', 32);
 
-$app_url = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . $_SERVER['HTTP_HOST']
-  . str_replace('setup.php', '', $_SERVER['REQUEST_URI']);
+// Check for existing config
+$configfile = __DIR__ . '/config.php';
+$configured = file_exists($configfile);
 
-if (function_exists('random_bytes')) {
-    $bytes = random_bytes(RANDOM_BYTE_COUNT);
-    $strong_crypto = true;
-} elseif (function_exists('openssl_random_pseudo_bytes')) {
-    $bytes = openssl_random_pseudo_bytes(RANDOM_BYTE_COUNT, $strong_crypto);
-} else {
-    $bytes = '';
-    for ($i=0; $i < RANDOM_BYTE_COUNT; $i++) {
-        $bytes .= chr(mt_rand(0, 255));
-    }
-    $strong_crypto = false;
-}
-$app_key = bin2hex($bytes);
-
-$configfile= __DIR__ . '/config.php';
-
-$configured = true;
-
-if (file_exists($configfile)) {
-    include_once $configfile;
-
-    if ((!defined('APP_URL') || APP_URL == '')
-        || (!defined('APP_KEY') || APP_KEY == '')
-        || (!defined('USER_HASH') || USER_HASH == '')
-        || (!defined('USER_URL') || USER_URL == '')
-    ) {
-        $configured = false;
-    }
-} else {
-    $configured = false;
-}
-
-if ($configured) : ?>
-    <h2>System already configured</h2>
-    <div class="instructions">
-        If you with to reconfigure, please remove config.php and reload this page.
-    </div>
-
-<?php else : ?>
-    <?php if ($strong_crypto === false) : ?>
-        <h2>
-           WARNING: this version of PHP does not support functions 'random_bytes' or 'openssl_random_pseudo_bytes'. 
-           This means your application is not as secure as it could be.  You may continues, but it is strongly recommended you upgrade PHP.
-        </h2> 
-    <?php endif; ?>
-
-    <div class="instructions">In order to configure Selfauth, you need to fill in a few values, this page helps generate those options.</div>
-    <?php if (isset($_POST['username'])) : ?>
-    <div>
-    <?php
+if (isset($_POST['username']) && isset($_POST['password']) && !$configured) {
+    $app_key = bin2hex(random_bytes(RANDOM_BYTE_COUNT));
+    $user_url = $_POST['username'];
+    // Hash using BCrypt
+    $user_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
     $app_url = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . $_SERVER['HTTP_HOST'] . str_replace('setup.php', '', $_SERVER['REQUEST_URI']);
 
-    $user = $_POST['username'];
+    $contents = "<?php\n";
+    $contents .= "define('APP_URL', '" . addslashes($app_url) . "');\n";
+    $contents .= "define('APP_KEY', '$app_key');\n";
+    $contents .= "define('USER_HASH', '" . addslashes($user_hash) . "');\n";
+    $contents .= "define('USER_URL', '" . addslashes($user_url) . "');\n";
 
-    $user_tmp = trim(preg_replace('/^https?:\/\//', '', $_POST['username']), '/');
-    $pass = md5($user_tmp . $_POST['password'] . $app_key);
-
-    $config_file_contents = "<?php
-define('APP_URL', '$app_url');
-define('APP_KEY', '$app_key');
-define('USER_HASH', '$pass');
-define('USER_URL', '$user');";
-
-    $file_written = false;
-
-    if (is_writeable($configfile) && !$configured) {
-        $handle = fopen($configfile, 'w');
-
-        if ($handle) {
-            $result = fwrite($handle, $config_file_contents);
-            if ($result !== false) {
-                $file_written = true;
-            }
-        }
-
-        fclose($handle);
-    }
-
-
-    if ($file_written) {
-        echo '<div class="message">config.php was successfully written to disk</div>';
+    if (is_writable(__DIR__)) {
+        file_put_contents($configfile, $contents);
+        $success = "Config saved successfully! Please delete setup.php now.";
     } else {
-        echo '<div class="message">Fill in the file config.php with the following content</div>';
-        echo '<pre>';
-        echo htmlentities($config_file_contents);
-        echo '</pre>';
+        $manual_config = $contents;
     }
+}
 ?>
-    </div>
-    <?php endif ?>
-    <form method="POST" action="">
-    <div class="form-line"><label>Login Url:</label> <input name='username' /></div>
-    <div class="form-line"><label>Password:</label> <input type='password' name='password' /></div>
-    <div class="form-line"><input class="submit" type="submit" name="submit" value="Generate Config"/></div>
-    </form>
-<?php endif; ?>
+<!doctype html>
+<html>
+<head>
+    <title>Setup Selfauth</title>
+    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+    <style>body{max-width:600px;margin:50px auto; text-align:center;}</style>
+</head>
+<body>
+    <h1>Setup Selfauth</h1>
+    <?php if ($configured): ?>
+        <p>System already configured. Delete <code>config.php</code> to reset.</p>
+    <?php elseif (isset($success)): ?>
+        <p style="color:green"><?php echo $success; ?></p>
+    <?php elseif (isset($manual_config)): ?>
+        <p>Could not write file. Create <code>config.php</code> manually with:</p>
+        <pre><?php echo htmlspecialchars($manual_config); ?></pre>
+    <?php else: ?>
+        <form method="POST">
+            <label>Your Personal URL (e.g. https://example.com)</label>
+            <input name="username" type="url" required placeholder="https://..." />
+            <label>Set Password</label>
+            <input name="password" type="password" required />
+            <br><br>
+            <button type="submit" style="width:100%">Generate Configuration</button>
+        </form>
+    <?php endif; ?>
 </body>
 </html>
